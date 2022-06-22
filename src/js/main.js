8f28177cbc841cd60d7d308792fc4c0d19b10fe7
CmdrMaylee <meleechan@gmail.com>
2022-06-22 14:37:32 +0200

    TEST PAGES removed method

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,30 @@
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener("DOMContentLoaded", main);
 const activeDate = new Date();
 let selectedDateForTodos = "";
 todos = JSON.parse(localStorage.getItem("data")) || [];
 
 function main() {
-    // Startup
-    createLocalStorageIfMissing();
+  // Startup
+  // createLocalStorageIfMissing();
 
-    // Todos
+  // Todos
 
+  // Calendar
+  addEventListeners(activeDate);
+  renderCalendarView();
 
-    // Calendar
-    addEventListeners(activeDate);
-    renderCalendarView();
-
-    // Header
-    headerCountNumOfTotalTodos();
+  // Header
+  headerCountNumOfTotalTodos();
 }
 
 function addEventListeners(date) {
-    const previousArrow = document.getElementById("previous-month-arrow");
-    previousArrow.addEventListener('click', function () { decrementMonth(date) });
+  const previousArrow = document.getElementById("previous-month-arrow");
+  previousArrow.addEventListener("click", function () {
+    decrementMonth(date);
+  });
 
-    const nextArrow = document.getElementById("next-month-arrow");
-    nextArrow.addEventListener('click', function () { incrementMonth(date) });
+  const nextArrow = document.getElementById("next-month-arrow");
+  nextArrow.addEventListener("click", function () {
+    incrementMonth(date);
+  });
 }
